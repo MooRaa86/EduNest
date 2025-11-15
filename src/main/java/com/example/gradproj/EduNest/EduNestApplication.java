d8f0e9b776928a23a9d16d5e15c6b9fe89869e42
Maro <mr.omarmedhat@gmail.com>
2025-11-15 15:33:14 +0200

    initialize the project

---
+++ src/main/java/com/example/gradproj/EduNest/EduNestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradproj.EduNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduNestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gradproj/EduNest/EduNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradproj.EduNest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
