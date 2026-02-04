b9a73feb08bd9e11abbaede2b8fc87ce7da9b7c7
Maro <mr.omarmedhat@gmail.com>
2026-02-04 13:35:36 +0200

    cors

--- src/main/java/com/example/gradproj/EduNest/config/ProjectSecurityProdconfig.java
+++ src/main/java/com/example/gradproj/EduNest/config/ProjectSecurityProdconfig.java
@@ -48,7 +48,7 @@ public class ProjectSecurityProdconfig {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(Collections.singletonList("*"));
+                        config.setAllowedOriginPatterns(Arrays.asList("*"));
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
